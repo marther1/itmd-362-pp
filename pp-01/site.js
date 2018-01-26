9e75391e1ff27a1a8c3d7c4a33b0bb482b1acc10
King Arther <kingarther2@hotmail.com>
2018-01-25 22:21:42 -0600

    alert the user if the phone number is not 10 digits

--- pp-01/site.js
+++ pp-01/site.js
@@ -2,6 +2,9 @@ document.getElementById('submit').addEventListener('click', check, false)
 function check(){
   var c = document.getElementById('phone').value;
   if (isNaN(c)){
-    alert('Please enter numbers!')
+    alert('Please enter numbers!');
+  }
+  else if (c!=10) {
+    alert('Please enter a 10 digit phone number');
   }
 }

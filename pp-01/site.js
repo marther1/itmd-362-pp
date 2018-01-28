a5787ac6d2d021d3ec5d8f2477b2cfc8c6a8d6a4
King Arther <kingarther2@hotmail.com>
2018-01-28 13:05:50 -0600

    use an anonymous function with displaying the alert in the console log instead of pop-up windows

--- pp-01/site.js
+++ pp-01/site.js
@@ -1,13 +1,12 @@
-document.getElementById('submit').addEventListener('click', check, false)
-function check(){
+document.getElementById('submit').addEventListener('click', function(){
   var c = document.getElementById('phone').value;
   if (isNaN(c)){
-    alert('Please enter numbers!');
+    console.log('Please enter numbers!');
   }
-  else if (c.length!=10) {
-    alert('Please enter a 10 digit phone number');
+  else if (c.length!==10) {
+    console.log('Please enter a 10 digit phone number');
   }
   else {
-    alert('Your form has been successfully submitted');
+    console.log('Your form has been successfully submitted');
   }
-}
+}, false);
